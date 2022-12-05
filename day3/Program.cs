a9b39bb8b67a4a1e7f223f8b580692d94a62836c
Noora Backlund <noora.backlund@gmail.com>
2022-12-06 00:35:40 +0200

    Added day 3 solution

---
+++ day3/Program.cs
@@ -0,0 +1,26 @@
+﻿var data = File.ReadAllLines("input.txt");
+
+var commonChars = new List<char>();
+var priorities = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Part 1
+foreach (var line in data)
+{
+    var mid = line.Length / 2;
+    var left = line.Substring(0, mid);
+    var right = line.Substring(mid, mid);
+    commonChars.AddRange(left.Intersect(right));
+}
+
+System.Console.WriteLine($"Sum of priorities (part 1): {commonChars.Sum(c => priorities.IndexOf(c))}");
+
+// Part 2
+var commonCharsPt2 = new List<char>();
+while (data.Length > 2)
+{
+    var group = data.Take(3);
+    data = data.Skip(3).ToArray();
+    commonCharsPt2.AddRange(group.ElementAt(0).Intersect(group.ElementAt(1)).Intersect(group.ElementAt(2)));   
+}
+
+System.Console.WriteLine($"Sum of priorities (part 2): {commonCharsPt2.Sum(c => priorities.IndexOf(c))}");
