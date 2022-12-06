86c6b8b0a0e9230fc05f1bb3ed70e6a9672792a0
Noora Backlund <noora.backlund@gmail.com>
2022-12-06 11:45:59 +0200

    Added part 1 + 2 solutions

---
+++ day6/Program.cs
@@ -0,0 +1,30 @@
+﻿var message = File.ReadAllLines("input.txt").First();
+
+// Part 1
+var markerFound =  false;
+var startPos = -1;
+do
+{
+    startPos++;
+    markerFound = message.Skip(startPos).Take(4).Distinct().Count() == 4;
+} while (!markerFound && startPos < message.Length);
+
+System.Console.WriteLine($"==== Part 1: ====");
+System.Console.WriteLine($"Marker found, starting at position {startPos}");
+System.Console.WriteLine($"Marker: {String.Join("", message.Skip(startPos).Take(4))}");
+System.Console.WriteLine($"Number of characters until marker end: {startPos + 4}");
+System.Console.WriteLine("");
+
+// Part 2
+startPos = 0;
+do
+{
+    startPos++;
+    markerFound = message.Skip(startPos).Take(14).Distinct().Count() == 14;
+} while (!markerFound && startPos < message.Length);
+
+System.Console.WriteLine($"==== Part 2: ====");
+System.Console.WriteLine($"Marker found, starting at position {startPos}");
+System.Console.WriteLine($"Marker: {String.Join("", message.Skip(startPos).Take(14))}");
+System.Console.WriteLine($"Number of characters until marker end: {startPos + 14}");
+System.Console.WriteLine("");
